94c2e80ee79a88a38888928493982b576561d4da
Pedro Baracho <pedropbaracho@gmail.com>
2016-12-10 22:17:18 -0200

    Added solution for "Quiz: Deploying Clustering"

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn.cluster import KMeans
 
 
 
@@ -53,6 +54,9 @@ features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
+kmeans = KMeans(n_clusters=2)
+kmeans = kmeans.fit(finance_features)
+pred = kmeans.predict(finance_features)
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
