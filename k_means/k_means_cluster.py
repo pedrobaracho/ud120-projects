dc8b909f3b86574b2b0bf272a05f5df0d7c50ac2
Pedro Baracho <pedropbaracho@gmail.com>
2016-12-10 22:22:22 -0200

    Moved clustering code to correct position. Renamed the output file

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -54,10 +54,6 @@ features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
-kmeans = KMeans(n_clusters=2)
-kmeans = kmeans.fit(finance_features)
-pred = kmeans.predict(finance_features)
-
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
@@ -69,12 +65,14 @@ plt.show()
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 
-
+kmeans = KMeans(n_clusters=2)
+kmeans = kmeans.fit(finance_features)
+pred = kmeans.predict(finance_features)
 
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters-2-features.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
