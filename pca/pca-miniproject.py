f0c0b1fcf4ee2b99301ef1e5e27e44635f54e5bc
Pedro Baracho <pedropbaracho@gmail.com>
2016-12-15 00:58:15 -0200

    Added solution for "Explained Variance for each PC"

---
+++ pca/pca-miniproject.py
@@ -0,0 +1,63 @@
+from time import time
+import logging
+import numpy as np
+
+from sklearn.cross_validation import train_test_split
+from sklearn.datasets import fetch_lfw_people
+from sklearn.decomposition import RandomizedPCA
+
+
+# Display progress logs on stdout
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+
+
+###############################################################################
+# Download the data, if not already on disk and load it as numpy arrays
+lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
+
+# introspect the images arrays to find the shapes (for plotting)
+n_samples, h, w = lfw_people.images.shape
+np.random.seed(42)
+
+# for machine learning we use the data directly (as relative pixel
+# position info is ignored by this model)
+X = lfw_people.data
+n_features = X.shape[1]
+
+# the label to predict is the id of the person
+y = lfw_people.target
+target_names = lfw_people.target_names
+n_classes = target_names.shape[0]
+
+print "Total dataset size:"
+print "n_samples: %d" % n_samples
+print "n_features: %d" % n_features
+print "n_classes: %d" % n_classes
+
+
+###############################################################################
+# Split into a training and testing set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+
+###############################################################################
+# Compute a PCA (eigenfaces) on the face dataset (treated as unlabeled
+# dataset): unsupervised feature extraction / dimensionality reduction
+n_components = 150
+
+print "Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0])
+t0 = time()
+pca = RandomizedPCA(n_components=n_components, whiten=True).fit(X_train)
+print "done in %0.3fs" % (time() - t0)
+
+eigenfaces = pca.components_.reshape((n_components, h, w))
+
+print "Projecting the input data on the eigenfaces orthonormal basis"
+t0 = time()
+X_train_pca = pca.transform(X_train)
+X_test_pca = pca.transform(X_test)
+print "done in %0.3fs" % (time() - t0)
+
+
+print "Explained Variance by PCA components:"
+print "#1: ", pca.explained_variance_ratio_[0]
+print "#2: ", pca.explained_variance_ratio_[1]
